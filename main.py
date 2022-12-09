d243a28faf58b13a178b79d2fa6218e7533a6647
Luca Lombardo <l.lombardo@protonmail.com>
2022-12-10 00:18:12 +0100

    C++ script that creates the checkins graph. Working, needs to be implemented with argparse-like something and multithreading

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# /bin/env/python3
-
-from itertools import combinations
-import os
-import wget
-import zipfile
-import networkx as nx
-import pandas as pd
-from typing import Literal
-
-
 """
 NOTEs:
 
@@ -17,6 +6,15 @@ NOTEs:
 - Why do I use os.path.join and not the "/"? Because it's more portable, it works on every OS, while "/" works only on Linux and Mac. If you want to use it on Windows, you have to change all the "/" with "\". With os.path.join you don't have to worry about it and, as always, f*** Microsoft.
 """
 
+import os
+import wget
+import zipfile
+import pandas as pd
+import networkx as nx
+from typing import Literal
+from itertools import combinations
+
+# ------------------------------------------------------------------------#
 
 def download_datasets():
 
@@ -39,23 +37,21 @@ def download_datasets():
         if not os.path.exists(os.path.join("data", folder)):
             os.mkdir(os.path.join("data", folder))
 
-    # download every url in urls[0] in the brightkite folder, and every url in urls[1] in the gowalla folder, and every url in urls[2] in the foursquare folder. At ech iteration, it checks if the file already exists, if yes, it skips the download and prints a message. If no, it downloads the file and prints a message.
+    # download every url in urls[0] in the brightkite folder, and every url in urls[1] in the gowalla folder, and every url in urls[2] in the foursquare folder. If the file is already downloaded, skip the download
 
     for i in range(len(urls)):
         for url in urls[i]:
-            # check if there are .txt files inside folder, if yes, skip the download
-            if len([file for file in os.listdir(os.path.join("data", folders[i])) if file.endswith(".txt")]) > 0:
-                print("The {} dataset is already downloaded and extracted as .txt file, if you want to download again the .gz file with this function, delete the .txt files in the folder".format(folders[i]))
-                break
-            # check if there are .gz files inside folder, if yes, skip the download
-            elif len([file for file in os.listdir(os.path.join("data", folders[i])) if file.endswith(".gz")]) > 0:
-                print("The {} dataset is already downloaded as .gz file, if you want to download again the .gz file with this function, delete the .gz files in the folder".format(folders[i]))
-                break
-            # if there are no .txt or .gz files, download the file
-            else:
-                print("Downloading {} dataset...".format(folders[i]))
+            if not os.path.exists(os.path.join("data", folders[i], url.split("/")[-1])):
                 wget.download(url, os.path.join("data", folders[i]))
-                print("Download completed of {} dataset".format(folders[i]))
+
+    # unzip the .gz files inside the brightkite and gowalla folders
+    for file in os.listdir(os.path.join("data", "brightkite")):
+        if file.endswith(".gz"):
+            os.system("gunzip {}".format(os.path.join("data", "brightkite", file)))
+
+    for file in os.listdir(os.path.join("data", "gowalla")):
+        if file.endswith(".gz"):
+            os.system("gunzip {}".format(os.path.join("data", "gowalla", file)))
 
     # extract the data of foursquare in a nice way, checking all the edge cases as a maniac. More details below
 
@@ -84,20 +80,50 @@ def download_datasets():
                     os.rename(os.path.join("data", "foursquare", "dataset_tsmc2014", file), os.path.join("data", "foursquare", file))
                 os.rmdir(os.path.join("data", "foursquare", "dataset_tsmc2014"))
 
-    # if there are no .txt files inside the brightkite folder, unzip the .gz files
-    if len([file for file in os.listdir(os.path.join("data", "brightkite")) if file.endswith(".txt")]) == 0:
-        for file in os.listdir(os.path.join("data", "brightkite")):
-            if file.endswith(".gz"):
-                os.system("gunzip {}".format(os.path.join("data", "brightkite", file)))
+    # Now we want to clean our data. Both for brightkite and gowalla, we want to rename _edges files as "brightkite_friends_edges.txt" and "gowalla_friends_edges.txt"
 
-    # if there are no .txt files inside the gowalla folder, unzip the .gz files
-    if len([file for file in os.listdir(os.path.join("data", "gowalla")) if file.endswith(".txt")]) == 0:
-        for file in os.listdir(os.path.join("data", "gowalla")):
-            if file.endswith(".gz"):
-                os.system("gunzip {}".format(os.path.join("data", "gowalla", file)))
+    for file in os.listdir(os.path.join("data", "brightkite")):
+        if file.endswith("_edges.txt"):
+            os.rename(os.path.join("data", "brightkite", file), os.path.join("data", "brightkite", "brightkite_friends_edges.txt"))
 
+    for file in os.listdir(os.path.join("data", "gowalla")):
+        if file.endswith("_edges.txt"):
+            os.rename(os.path.join("data", "gowalla", file), os.path.join("data", "gowalla", "gowalla_friends_edges.txt"))
 
-def create_checkins_graph(dataset: Literal['brightkite', 'gowalla', 'foursquareNYC', 'foursquareTKY'])-> nx.Graph:
+    # Now we from the _totalCheckins.txt files we want to keep only the first and last column, which are the user ID and the venue ID. We also want to remove the header of the file. Use pandas to do that. Then rename the files as "brightkite_checkins_edges.txt" and "gowalla_checkins_edges.txt
+
+    for file in os.listdir(os.path.join("data", "brightkite")):
+        if file.endswith("_totalCheckins.txt"):
+            df = pd.read_csv(os.path.join("data", "brightkite", file), sep="\t", header=None)
+            df = df[[0, 2]]
+            df.to_csv(os.path.join("data", "brightkite", "brightkite_checkins.txt"), sep="\t", header=False, index=False, errors="ignore", encoding="utf-8")
+            os.remove(os.path.join("data", "brightkite", file))
+
+    for file in os.listdir(os.path.join("data", "gowalla")):
+        if file.endswith("_totalCheckins.txt"):
+            df = pd.read_csv(os.path.join("data", "gowalla", file), sep="\t", header=None)
+            df = df[[0, 2]]
+            df.to_csv(os.path.join("data", "gowalla", "gowalla_checkins.txt"), sep="\t", header=False, index=False, errors="ignore", encoding="utf-8")
+            os.remove(os.path.join("data", "gowalla", file))
+
+    # now for foursquare we want to keep only the first and second column, which are the user ID and the venue ID. We also want to remove the header of the file. Use pandas to do that. Do that for both _NYC.txt and _TKY.txt files. Then rename the files as "foursquare_checkins_edges_NYC.txt" and "foursquare_checkins_edges_TKY.txt
+
+    for file in os.listdir(os.path.join("data", "foursquare")):
+        if file.endswith("_NYC.txt"):
+            df = pd.read_csv(os.path.join("data", "foursquare", file), sep="\t", header=None, encoding="utf-8", encoding_errors="ignore")
+            df = df[[0, 1]]
+            df.to_csv(os.path.join("data", "foursquare", "foursquare_checkins_NYC.txt"), sep="\t", header=False, index=False)
+            os.remove(os.path.join("data", "foursquare", file))
+
+        if file.endswith("_TKY.txt"):
+            df = pd.read_csv(os.path.join("data", "foursquare", file), sep="\t", header=None, encoding="utf-8", encoding_errors="ignore")
+            df = df[[0, 1]]
+            df.to_csv(os.path.join("data", "foursquare", "foursquare_checkins_TKY.txt"), sep="\t", header=False, index=False)
+            os.remove(os.path.join("data", "foursquare", file))
+
+# ------------------------------------------------------------------------#
+
+def create_checkins_graph_SLOW(dataset: Literal['brightkite', 'gowalla', 'foursquareNYC', 'foursquareTKY'])-> nx.Graph:
 
     """
     This function takes in input a tsv file, each line in the file is a check-in. The function returns an undirected networkx graph object.
@@ -136,6 +162,8 @@ def create_checkins_graph(dataset: Literal['brightkite', 'gowalla', 'foursquareN
 
     print("Number of edges added to the graph {}: {}".format(dataset, G.number_of_edges()))
 
+# ------------------------------------------------------------------------#
+
 def friendships_graph(dataset: Literal['brightkite', 'gowalla']) -> nx.Graph:
 
     """
@@ -152,7 +180,41 @@ def friendships_graph(dataset: Literal['brightkite', 'gowalla']) -> nx.Graph:
     if dataset not in ["brightkite", "gowalla"]:
         raise ValueError("The dataset must be brightkite or gowalla")
 
-    file = os.path.join("data", dataset, "loc-{}_edges.txt".format(dataset))
+    file = os.path.join("data", dataset, "{}_friends_edges.txt".format(dataset))
+    df = pd.read_csv(file, sep="\t", header=None, names=["node1", "node2"])
+    G = nx.from_pandas_edgelist(df, "node1", "node2", create_using=nx.Graph())
+
+    return G
+
+# ------------------------------------------------------------------------#
+
+def checkins_graph_from_edges(dataset: Literal['brightkite', 'gowalla', 'foursquareNYC', 'foursquareTKY']) -> nx.Graph:
+
+    """
+    This function takes in input a tsv file with two columns, Each line in the file is an edge. The function returns an undirected networkx graph object. It uses pandas to read the file since it's faster than the standard python open() function. If we don't want to use the standard python open() function, the following code works as well:
+
+    G = nx.Graph()
+    with open(file, "r") as f:
+        for line in f:
+            node1, node2 = line.split("\t")
+            G.add_edge(node1, node2)
+
+    """
+
+    if dataset not in ["brightkite", "gowalla", "foursquareNYC", "foursquareTKY"]:
+        raise ValueError("The dataset must be brightkite, gowalla or foursquare")
+
+
+    file = os.path.join("data", dataset, "{}_checkins_edges.tsv".format(dataset))
+
+    # if dataset == "foursquareTKY":
+    #     file = os.path.join("data", "foursquare", "foursquareNYC_checkins_graph.tsv")
+    # elif dataset == "foursquareNYC":
+    #     file = os.path.join("data", "foursquare", "foursquareTKY_checkins_graph.tsv")
+    # else:
+    #     file = os.path.join("data", dataset, "{}_checkins_graph.tsv".format(dataset))
+
+
     df = pd.read_csv(file, sep="\t", header=None, names=["node1", "node2"])
     G = nx.from_pandas_edgelist(df, "node1", "node2", create_using=nx.Graph())
 
